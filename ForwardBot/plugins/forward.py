e52831eb3ccc8a99f21e533a4b160c205940873f
boxxoA <francihula12@gmail.com>
2022-08-29 21:43:25 +0200

    No updatesDifference

--- ForwardBot/plugins/forward.py
+++ ForwardBot/plugins/forward.py
@@ -3,12 +3,10 @@ import re
 from typing import Optional
 
 import pyrogram
-from pyrogram import enums
-from pyrogram.enums import ParseMode
-from pyrogram.raw.functions.updates import GetState, GetDifference
+
 from pyrogram.types import MessageEntity
 
-from ForwardBot import bot, Config, LOGS, collezione_get, collezione_fw, unwrap_dict, BotConfig
+from ForwardBot import bot, Config, LOGS, collezione_get, collezione_fw,  BotConfig
 from ForwardBot.SymbConfig import Symb_Config, BlacklistWords
 from ForwardBot.events import register, message_deleted
 
@@ -372,17 +370,17 @@ async def handler(event: pyrogram.types.List):
     LOGS.info(f"----MESSAGE DELETED EVENT CAPTURED BLOCK END----")
 
 
-@pyrogram.Client.on_raw_update()
-async def handler(event):
-    LOGS.info(f"----RAW UPDATE EVENT CAPTURED BLOCK START----")
-    if isinstance(event, pyrogram.raw.types.UpdatesTooLong):
-        # handle the update by calling GetDifference
-        getstate = await bot.invoke(
-            GetState()
-        )
-
-        await bot.invoke(
-            GetDifference(pts=getstate.pts, date=getstate.date, qts=getstate.qts))
-        LOGS.info(f"pts: {getstate.pts}, date: {getstate.date}, qts: {getstate.qts}")
-
-        LOGS.info(f"----RAW UPDATE EVENT CAPTURED BLOCK END----")
+# @pyrogram.Client.on_raw_update()
+# async def handler(event):
+#     LOGS.info(f"----RAW UPDATE EVENT CAPTURED BLOCK START----")
+#     if isinstance(event, pyrogram.raw.types.UpdatesTooLong):
+#         # handle the update by calling GetDifference
+#         getstate = await bot.invoke(
+#             GetState()
+#         )
+#
+#         test=await bot.invoke(
+#             GetDifference(pts=getstate.pts, date=getstate.date, qts=getstate.qts))
+#         LOGS.info(f"pts: {getstate.pts}, date: {getstate.date}, qts: {getstate.qts}")
+#
+#         LOGS.info(f"----RAW UPDATE EVENT CAPTURED BLOCK END----")
--- ForwardBot/plugins/updates.py
+++ ForwardBot/plugins/updates.py
@@ -1,7 +1,6 @@
 from datetime import datetime
 
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
-
 import ForwardBot
 from ForwardBot import Config
 
@@ -14,7 +13,9 @@ async def send_ping_message(chat_id=Config.PING_CHANNEL_ID):
     try:
       await ForwardBot.bot.send_message(chat_id=chat_id, text=f"msg_polling {current_time}")
     except BaseException as e:
-        ForwardBot.LOGS.info(e)
+        ForwardBot.LOGS.error(e)
+
+
 
 SCHEDULER.add_job(send_ping_message, 'interval', minutes=60)
 SCHEDULER.start()
